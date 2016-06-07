606bb373f2138b420c1860588300c01225604a2d
Jiawei Song <songjiawei24@gmail.com>
2016-06-06 19:47:57 -0700

    updated repo info in package.json | update server.js to app.js

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path');
 
 app.use(express.static('public')); // static files
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('cross road game listening on port 3000!');
 });
